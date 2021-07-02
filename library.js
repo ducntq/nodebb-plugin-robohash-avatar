8bd84740245856dd5d06ca3a59e2b52a91bff208
Duc Nguyen <ducntq@gmail.com>
2021-07-03 00:32:19 +0700

    Working now

--- library.js
+++ library.js
@@ -1,20 +1,16 @@
 'use strict';
 
-const winston = require.main.require('winston');
 var User = require.main.require('./src/user');
 
 const plugin = {};
 
-plugin.process = async (uid, next) => {
-	const userslug = await User.getUserField(uid, 'userslug');
-	let avatar = `https://robohash.org/${userslug}.png`;
-	winston.info(`[plugins/robo-hash] Set avatar for user ${userslug} to ${avatar}`);
-	await User.updateProfile(uid, {
-		uid: uid,
-		uploadedpicture: avatar,
-		picture: avatar,
-	}, ['uploadedpicture', 'picture']);
-	return {uid: uid, next: next};
+plugin.process = async (data) => {
+        const userslug = await User.getUserField(data.uid, 'userslug');
+        let avatar = `https://robohash.org/${userslug}.png`;
+        await User.updateProfile(data.uid, {
+                uid: data.uid,
+                uploadedpicture: avatar,
+                picture: avatar,
+        }, ['uploadedpicture', 'picture']);
+        return data;
 };
-
-module.exports = plugin;
